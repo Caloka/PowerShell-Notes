29db1df82f58f27d7da165bbbd880f1862816b4c
Calil Gomes <75040393+Caloka@users.noreply.github.com>
2023-07-30 10:43:13 -0300

    Update PRIEMIROSCRIPTPOWERSHELL.ps1

--- PRIEMIROSCRIPTPOWERSHELL.ps1
+++ PRIEMIROSCRIPTPOWERSHELL.ps1
@@ -1,6 +1,6 @@
 # Starting our script to extract bases in the website called hemera
 
-$Path_files = 'C:\Users\B355992\Desktop\TESTES NO POWERSHELL\Chrome driver'
+$Path_files = 'C:\Users\Desktop\TESTES NO POWERSHELL\Chrome driver'
 $Path_file_dll = '$(Path_files)\WebDriver.dll'
 
 Write-Host $Path_file_dll
